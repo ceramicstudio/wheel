49f1aba6dd87329610fdfe6bab82e96c09a29d21
Danny Browning <dbrowning@3box.io>
2023-02-07 10:55:35 -0700

    Fixes for script, bump versions for release

--- wheel.sh
+++ wheel.sh
@@ -27,18 +27,19 @@ then
 fi
 TARGET=$ARCH-$OS
 
-echo "Downloading wheel for arch "TARGET
-
 VERSION=$(curl https://api.github.com/repos/$REPO -s |  jq .name -r)
 TAR_NAME=wheel-$VERSION-$TARGET
 OUTPUT_FILE=wheel.tar.xz
+DOWNLOAD_URL=https://github.com/$REPO/download/$TAR_NAME.tar.xz
 
-curl -L0 https://github.com/$REPO/download/$TAR_NAME.tar.xz --output $OUTPUT_FILE
+echo "Downloading wheel for target "$TARGET" from "$DOWNLOAD_URL
+
+curl -LJO --output $OUTPUT_FILE $DOWNLOAD_URL
 
 tar -xvf $OUTPUT_FILE
 rm $OUTPUT_FILE
 
-mv ./$TAR_NAME ./wheel
+mv $TAR_NAME wheel
 
 cd wheel
 
